635eb770dde3028d5b349a9122297bed25e582e8
dinesh-prajwal <dineshveerapareddy@prajwalfintech.com>
2025-10-18 22:44:07 +0530

    Refactor Home component by simplifying the container structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Projects from "@/sections/Projects";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div>
       <Header />
       <div id="home">
       <Herosection />
